366a02fba1e6fd021185310524909123c8b0d9b0
Joshi <amarnath@foradian.com>
2019-12-08 16:43:22 +0530

    basic setups

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :ratings
+  resources :interests
+  resources :leads
+  resources :references
+  resources :students
+  resources :courses
+  resources :colleges
+  resources :agents
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
