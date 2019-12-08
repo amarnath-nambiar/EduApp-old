89515a0b3787cdd26850c3d919e9bc758345ae85
Joshi <amarnath@foradian.com>
2019-12-08 21:12:56 +0530

    Basic setups

--- db/migrate/20191130172018_create_agents.rb
+++ db/migrate/20191130172018_create_agents.rb
@@ -3,7 +3,6 @@ class CreateAgents < ActiveRecord::Migration[5.2]
     create_table :agents do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
       t.string :encrypted_phone
       t.string :encrypted_alt_phone
       t.string :about
